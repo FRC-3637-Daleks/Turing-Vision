a00228c54ffa46e4e1c93b6d9181f390449f11ea
jbott <snowleopardjb@gmail.com>
2015-02-08 04:05:52 +0000

    Clean up basic code

--- vision.cpp
+++ vision.cpp
@@ -13,22 +13,27 @@ int main(int argc, char **argv)
 	if(!cap.isOpened())
 		return -1;
 
+	cout << "Camera opened" << endl;
+	cout << "Reading first frame..." << endl;
+	cv::Mat frame;
+	cap.read(frame);
+
+	cout << "First frame read, starting loop" << endl;
+
 	long count = 0;
 	Clock::time_point start, stop;
 	start = Clock::now();
 	while (1) {
-		cv::Mat frame;
+		count += 1;
 		cap.read(frame);
-		cv::imwrite("out.jpg", frame);
 
 		// Show framerate
-		if (count == 30) {
+		if (count % 30 == 0) {
+			cv::imwrite("out.jpg", frame);
 			stop = Clock::now();
-			cout << "FPS:" << double(count) /  chrono::duration_cast<chrono::seconds>(stop - start).count() << endl;
+			cout << "Frame: " << count << "\tFPS: " << 30 / double(chrono::duration_cast<chrono::milliseconds>(stop - start).count()) * 1000 << endl;
 			start = Clock::now();
-			count = 0;
 		}
-		count += 1;
 	}
 	return 0;
 }
